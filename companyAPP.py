91a5332553c854db4f2b53dfc9f65d81500dc4ce
KelvinSing <143489302+KelvinSing@users.noreply.github.com>
2023-09-19 03:07:42 +0800

    Add files via upload

---
+++ companyAPP.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template, request
+from pymysql import connections
+import os
+import boto3
+from config import *
+
+app = Flask(__name__)
+
+bucket = custombucket
+region = customregion
+
+db_conn = connections.Connection(
+    host=customhost,
+    port=3306,
+    user=customuser,
+    password=custompass,
+    db=customdb
+
+)
+output = {}
+table = 'Company_Profile'
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template('company-login.html')
+
+@app.route("/company-login", methods=['POST'])
+def AddEmp():
+    company_name = request.form['Company_Name']
+    company_email = request.form['Company_Email']
+    password = request.form['Password']
+    company_description = request.form['Company_Description']
+    company_address = request.form['Company_Address']
+    contact_number = request.form['Contact_Number']
+    website_URL = request.form['Website_URL']
+    industry = request.form['Industry']
+    company_size = request.form['Company_Size']
+    company_logo = request.files['Company_Logo']
+
+    insert_sql = "INSERT INTO Company_Profile VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor = db_conn.cursor()
+
+    if company_logo.filename == "":
+        return "Please select a file"
+
+    try:
+
+        cursor.execute(insert_sql, (company_name, company_email, password, company_description, company_address, contact_number, website_URL, industry, company_size))
+        db_conn.commit()
+        # Uplaod image file in S3 #
+        company_logo_in_s3 = str(company_name) + "_logo"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(Key=company_logo_in_s3, Body=company_logo)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                company_logo_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template('company-login.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
